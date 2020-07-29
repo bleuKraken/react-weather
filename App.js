2645dfaa1f2542b2b942e51ba3e3627c47c5a513
erickkg <erick31641@yahoo.com>
2020-07-28 23:06:15 -0700

    First!

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Platform } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Text label  */}
+      <Text style={[styles.largeText, styles.textStyle]}>Los Angeles</Text>
+      <Text style={[styles.smallText, styles.textStyle]}>Light Clouds</Text>
+      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
     </View>
   );
 }
 
+// Muh styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
   },
 });
